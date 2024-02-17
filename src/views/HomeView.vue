77be7ec07bc9c560f9894d445fa4a6ab40dee7ab
ccastro1016 <andycastro.001016@gmail.com>
2024-02-17 16:57:22 -0500

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import users from '@/stores/userData'
+  import { DataTable } from "@/../node_modules/DataTable"
+</script>
+
+<template>
+  <DataTable :rows="users"
+               sortable/>
+</template>
